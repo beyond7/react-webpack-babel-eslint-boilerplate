9597c147696ed776a84e47fb76544911c827c4f7
Brandon Skinner <bs.beyonddev@gmail.com>
2019-12-11 12:28:52 +0000

    React Webpack + ESLint + Babel

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # webpack_base
+
+React + Webpack + ESLint + DotENV + ...
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,86 +1,93 @@
 const webpack = require("webpack");
 const convert = require("koa-connect");
+const Dotenv = require("dotenv-webpack");
 const history = require("connect-history-api-fallback");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
+
 const commonPaths = require("./paths");
-const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-	entry: [commonPaths.entryPath, "react-hot-loader/patch"],
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				loader: require.resolve("babel-loader"),
-				options: {
-					// This is a feature of `babel-loader` for webpack (not Babel itself).
-					// It enables caching results in ./node_modules/.cache/babel-loader/
-					// directory for faster rebuilds.
-					// cacheDirectory: true,
-					plugins: ["react-hot-loader/babel"]
-				}
-			},
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: "html-loader"
-					}
-				]
-			},
-			{
-				test: /\.(png|jpg|gif|svg)$/,
-				use: [
-					{
-						loader: "file-loader",
-						options: {
-							outputPath: commonPaths.imagesFolder,
-							publicPath: "../" + commonPaths.imagesFolder
-						}
-					}
-				]
-			},
-			{
-				test: /\.(woff2|ttf|woff|eot)$/,
-				use: [
-					{
-						loader: "file-loader",
-						options: {
-							outputPath: commonPaths.fontsFolder,
-							publicPath: "../" + commonPaths.fontsFolder
-						}
-					}
-				]
-			},
-			{ test: /\.xlsx$/, loader: "webpack-xlsx-loader" }
-		]
-	},
-	serve: {
-		add: app => {
-			app.use(convert(history()));
-		},
-		content: commonPaths.entryPath,
-		dev: {
-			publicPath: commonPaths.outputPath
-		},
-		open: true
-	},
-	resolve: {
-		modules: ["src", "node_modules"],
-		extensions: ["*", ".js", ".jsx", ".css", ".scss"],
-		alias: {
-			'react-dom': '@hot-loader/react-dom'
-		}
-	},
-	plugins: [
-		new webpack.ProgressPlugin(),
-		new HtmlWebpackPlugin({
-			template: commonPaths.templatePath
-		}),
-		new ScriptExtHtmlWebpackPlugin({
-			defaultAttribute: "async"
-		}),
-		new Dotenv({ path: "./.env" })
-	]
+  entry: [commonPaths.entryPath, "react-hot-loader/patch"],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: require.resolve("babel-loader"),
+        options: {
+          plugins: ["react-hot-loader/babel"]
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: commonPaths.imagesFolder,
+              publicPath: "../" + commonPaths.imagesFolder
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff2|ttf|woff|eot)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: commonPaths.fontsFolder,
+              publicPath: "../" + commonPaths.fontsFolder
+            }
+          }
+        ]
+      },
+      {
+        test: /jquery-ui\/.+\.js$/,
+        use: {
+          loader: "imports-loader?jQuery=jquery,$=jquery,this=>window"
+        }
+      }
+    ]
+  },
+  serve: {
+    add: app => {
+      app.use(convert(history()));
+    },
+    content: commonPaths.entryPath,
+    dev: {
+      publicPath: commonPaths.outputPath
+    },
+    open: true
+  },
+  resolve: {
+    modules: ["src", "node_modules"],
+    extensions: ["*", ".js", ".jsx", ".css", ".scss"],
+    alias: {
+      "react-dom": "@hot-loader/react-dom"
+    }
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: commonPaths.templatePath
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: "async"
+    }),
+    new Dotenv({ path: "./.env" }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery"
+    })
+  ]
 };
